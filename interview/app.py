44843f6ae3d5269429e80d80ee4d44fb1928fa3e
Arturo Guerrero Lopez <aguerrero@wiggi.mx>
2025-06-12 15:32:08 -0600

    commets

--- interview/app.py
+++ interview/app.py
@@ -16,7 +16,7 @@ def index():
 @app.route("/audit")
 def audit():
     print("Audit started")
-    pnl_path = "https://gist.githubusercontent.com/arturxdev/e3f45f2a4ba49b450ad5361f755a5a74/raw/92b8fc20793d41ff6c98a7d34f6882e8967bba7b/pnl_202.csv"
+    pnl_path = "https://gist.githubusercontent.com/arturxdev/e3f45f2a4ba49b450ad5361f755a5a74/raw/92b8fc20793d41ff6c98a7d34f6882e8967bba7b/pnl_2025.csv"
     balance_path = "https://gist.githubusercontent.com/arturxdev/2b05e59f4920517537a09fc3fd9c22bd/raw/64c46863e80e87054fc28628f4dba68b75659154/balance_2025.csv"
     
     def background_task():
